77889e76a5967c144430829aad617b01e6846e28
SadMap <71943073+SadMap@users.noreply.github.com>
2024-07-19 12:21:11 +0300

    temp page

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,13 @@ import './Home.css'
 function Home() {
 
   return (
-        
+    <main className="page-wrapper border-bottom">
+      <div className="container">
+        <h1 className="text-center">Home</h1>
+        <p className="lead text-center">This site is under constraction</p>
+        <a href='https://bots.discord.uno' className="btn btn-primary">Discord Bots</a>
+      </div>
+    </main>
   )
 }
 
